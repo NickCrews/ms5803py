b144eb4485e681e25d8e38903f68d319940bf0a3
NickCrews <nicholas.b.crews@gmail.com>
2018-07-17 11:27:23 -0800

    made a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='MS5803-14BA-python',
+      version='0.1',
+      description='Python library for MS5803-14BA pressure sensor for Raspberry Pi',
+      url='https://github.com/NickCrews/MS5803-14BA-python',
+      author='Nick Crews',
+      author_email='nicholas.b.crews@gmail.com',
+      license='MIT',
+      packages=['MS5803-14BA-python'],
+      install_requires=["smbus"],
+      zip_safe=False)
