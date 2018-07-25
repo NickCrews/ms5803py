6ec8ffc8784a75fc01df6774a71194bb4256f6b6
NickCrews <nicholas.b.crews@gmail.com>
2018-07-25 15:58:58 -0800

    added README to pypi homepage

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='ms5803py',
-      version='0.1.2',
+      version='0.1.3',
       description='Python library for MS5803-14BA pressure sensor for Raspberry Pi',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/NickCrews/ms5803py',
       author='Nick Crews',
       author_email='nicholas.b.crews@gmail.com',
