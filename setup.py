4dcf41b16ec3ce96fd3e2a9ba266d42783e0c9c6
NickCrews <nicholas.b.crews@gmail.com>
2018-07-25 17:27:32 -0800

    added copyright and some tags

--- setup.py
+++ setup.py
@@ -11,9 +11,22 @@ setup(name='ms5803py',
       description='Python library for MS5803-14BA pressure sensor for Raspberry Pi',
       long_description=long_description,
       long_description_content_type='text/markdown',
+      copyright = 'Copyright (c) 2018 Nick Crews',
       url='https://github.com/NickCrews/ms5803py',
       author='Nick Crews',
       author_email='nicholas.b.crews@gmail.com',
       license='MIT',
       packages=['ms5803py'],
-      zip_safe=False)
+      zip_safe=False,
+      keywords = [],
+      classifiers = [
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering,
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Topic :: Utilities'
+        ])
